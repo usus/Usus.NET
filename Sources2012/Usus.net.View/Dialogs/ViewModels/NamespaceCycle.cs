460f72d2855d51bb1788ca1d8647fa5ce5de358b
Manuel Naujoks <mnaujoks@gmail.com>
2013-06-30 14:51:20 +0200

    Namespace cycles can now additionally be displayed with graphs.

--- Sources2012/Usus.net.View/Dialogs/ViewModels/NamespaceCycle.cs
+++ Sources2012/Usus.net.View/Dialogs/ViewModels/NamespaceCycle.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using andrena.Usus.net.Core.Helper;
+using andrena.Usus.net.Core.Reports;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using andrena.Usus.net.Core.Helper;
-using andrena.Usus.net.Core.Reports;
 
 namespace andrena.Usus.net.View.Dialogs.ViewModels
 {
@@ -25,12 +25,12 @@ namespace andrena.Usus.net.View.Dialogs.ViewModels
             get { return namespaces.Select(n => n.Name); }
         }
 
-        public IEnumerable<string> TypesReferencingOutOf(string namespaceName)
+		public IEnumerable<OutOfNamespaceReference> TypesReferencingOutOf(string namespaceName)
         {
             var currentNamespace = metrics.ForNamespace(namespaceName);
             var otherNamespaceInCycle = namespaces.ExceptThis(currentNamespace).ToList();
             return from reference in ReferencesOutOf(currentNamespace, to: otherNamespaceInCycle)
-                   select DisplayStringOf(reference);
+                   select reference;
         }
 
         private IEnumerable<OutOfNamespaceReference> ReferencesOutOf(NamespaceMetricsReport currentNamespace, IEnumerable<NamespaceMetricsReport> to)
@@ -44,21 +44,6 @@ namespace andrena.Usus.net.View.Dialogs.ViewModels
         {
             return from type in metrics.TypesOfNamespace(currentNamespace)
                    select new OutgoingTypeReferences(metrics, type);
-        }
-
-        private string DisplayStringOf(OutOfNamespaceReference reference)
-        {
-            return string.Format("{0}\n -> {1} ({2})",
-                reference.Source.FullName,
-                reference.Target.FullName,
-                MethodsWith(reference));
-        }
-
-        private string MethodsWith(OutOfNamespaceReference reference)
-        {
-            return string.Join(", ", from method in metrics.MethodsOfType(reference.Source)
-                                     where method.TypeDependencies.Contains(reference.Target.FullName)
-                                     select method.Name);
         }
 
         public void JumpTo(string type)
--- Sources2012/Usus.net.View/Dialogs/ViewModels/NamespaceCycleDisplayVM.cs
+++ Sources2012/Usus.net.View/Dialogs/ViewModels/NamespaceCycleDisplayVM.cs
@@ -1,20 +1,46 @@
+using andrena.Usus.net.View.ViewModels;
 using QuickGraph;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace andrena.Usus.net.View.Dialogs.ViewModels
 {
-	public class NamespaceCycleDisplayVM
+	public class NamespaceCycleDisplayVM : ViewModel
 	{
 		public string Header { get; private set; }
 		public ObservableCollection<string> MainEntries { get; private set; }
 		public ObservableCollection<string> SubEntries { get; private set; }
 		public IBidirectionalGraph<object, IEdge<object>> Graph { get; private set; }
 
+		private string layoutAlgorithmType;
+		public string LayoutAlgorithmType
+		{
+			get { return layoutAlgorithmType; }
+			set
+			{
+				layoutAlgorithmType = value;
+				Changed(() => LayoutAlgorithmType);
+			}
+		}
+		public IEnumerable<string> LayoutAlgorithmTypes { get; private set; }
+
 		private NamespaceCycle cycle;
 
 		public NamespaceCycleDisplayVM(string header)
 		{
+			LayoutAlgorithmTypes = new ObservableCollection<string> {
+				"BoundedFR", 
+				"Circular", 
+				"CompoundFDP",
+				"EfficientSugiyama", 
+				"FR", 
+				"ISOM", 
+				"KK", 
+				"LinLog", 
+				"Tree"
+			};
+
 			Header = header;
 			MainEntries = new ObservableCollection<string>();
 			SubEntries = new ObservableCollection<string>();
@@ -23,23 +49,34 @@ namespace andrena.Usus.net.View.Dialogs.ViewModels
 		public void Display(NamespaceCycle namespaceCycle)
 		{
 			cycle = namespaceCycle;
-			foreach (var namespaceInCycle in cycle.Namespaces) MainEntries.Add(namespaceInCycle);
-			
-			Graph = CreateExsampleGraph();
+
+			FillMainEntries();
+			FillGraph();
 		}
 
-		private static IBidirectionalGraph<object, IEdge<object>> CreateExsampleGraph()
+		private void FillMainEntries()
 		{
-			var graph = new BidirectionalGraph<object, IEdge<object>>();
-			graph.AddVertexRange(Enumerable.Range(1, 5).Select(i => (object)i));
-			graph.AddEdgeRange(Enumerable.Range(1, 4).Zip(Enumerable.Range(2, 5), (s, t) => new Edge<object>(s, t)));
-			return graph;
+			foreach (var namespaceInCycle in cycle.Namespaces) 
+				MainEntries.Add(namespaceInCycle);
 		}
 
+		private void FillGraph()
+		{
+			var graph = new BidirectionalGraph<object, IEdge<object>>(false);
+			graph.AddVertexRange(cycle.Namespaces);
+			graph.AddEdgeRange(
+				from namespaceInCycle in cycle.Namespaces
+				from outOfNamespaceReference in cycle.TypesReferencingOutOf(namespaceInCycle)
+				select new Edge<object>(namespaceInCycle, outOfNamespaceReference.TargetNamespace.Name));
+
+			Graph = graph;
+		}
+	
 		public void SelectNamespace(string namespaceName)
 		{
 			SubEntries.Clear();
-			foreach (var type in cycle.TypesReferencingOutOf(namespaceName)) SubEntries.Add(type);
+			foreach (var type in cycle.TypesReferencingOutOf(namespaceName))
+				SubEntries.Add(type.DisplayString());
 		}
 
 		public void SelectType(string type)
